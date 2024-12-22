8bc9ea3ecbd3d0ed5b1f3883f76cc626cc7ce162
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2024-12-22 19:32:58 +0530

    maine bhai commit firse kiya hai jisme .gitignore file env file mention kiya hai dekh lio

--- backend/server.js
+++ backend/server.js
@@ -15,3 +15,5 @@ server.listen(port, () => {
 // but hum directly server ko listen port par nahi kar sakte hai toh hum ek kaam karenge dotenv package install karenge and then dotenv package se hum ek environment file banaienge jiska naam ho ga .env and is file me hum maximum chije jaise ki
 // api keys bagaira hai rakhenge so  that ki koi bhi use direct access na kar pai
 // and hamne sath hi me ek cors package install kiya hai cors ka basically kaam ye hai ki ye cross origin resources ko share karne me help karta hai and then hum me help karta hai ki resources ko share kiye ja sake thik
+//  hame sabse pehle apne code me ek gitignore file banani hogi
+// gitignore file hame help karta hai ki ye jo bhi files hamare pass mentioned hoti hai gitignore me woh nahi jaiega commit ke sath github pe so that no one has access to our public repo env files
