56b3070fc0c66836d4085a7ac749cee61725176a
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2024-12-30 10:27:51 +0530

    usersignup complete kiya

--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -4,10 +4,17 @@ import { Link } from 'react-router-dom'
 const UserSignup = () => {
   const [email,setEmail] = useState("");
   const[password,setPassword] = useState("")
-  const[userData,setUserData]= useState({})
+  const [firstname, setFirstname] = useState("")
+  const [lastname, setLastname] = useState("")
+  const [userData,setUserData] = useState({})
   const submitHandler=(e)=>{
 e.preventDefault();
 setUserData({
+  username:{
+    firstname:firstname,
+    lastname:lastname
+  },
+  
   email:email,
   password:password
 })
@@ -15,6 +22,8 @@ console.log(userData);
 
 setEmail('');
 setPassword('');
+setFirstname('');
+setLastname('');
   }
   return (
     <div className='p-7 h-screen flex flex-col justify-between '>
@@ -23,31 +32,52 @@ setPassword('');
       <form onSubmit={(e)=>{
         submitHandler(e)
       }}>
+        <h3 className='text-lg font-medium mb-2'>What's your name</h3>
+          <div className="flex gap-4 mb-5">
+           <input 
+      required 
+      
+      className='bg-[#eeeeee]  rounded px-4 py-2 border w-1/2 text-lg placeholder:text-base'
+      type="text"
+       placeholder="First name"
+       value={firstname}
+        onChange={(e)=>setFirstname(e.target.value)}
+       />
+       
+         <input 
+      required 
+      
+      className='bg-[#eeeeee]   rounded px-4 py-2 border w-1/2 text-lg placeholder:text-base'
+      type="text"
+       placeholder="last name"
+       value={lastname}
+        onChange={(e)=>setLastname(e.target.value)}
+       />
+       
+       </div>
       <h3 className='text-lg font-medium mb-2'>What's your email</h3>
       <input 
       required 
       value={email}
-      onChange={(e)=>setEmail(e.target.value)}
-      className='bg-[#eeeeee]  mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base'
+        onChange={(e)=>setEmail(e.target.value)}
+      className='bg-[#eeeeee]  mb-5 rounded px-4 py-2 border w-full text-lg placeholder:text-base'
       type="email"
        placeholder="Pranav@example.com"/>
-      <h3 className='text-lg font-medium mb-2'>Enter Password</h3>
+      <h3 className='text-base font-medium mb-2'>Enter Password</h3>
       <input 
       value={password}
-      onChange={(e)=>setPassword(e.target.value)}
-      className='bg-[#eeeeee]  mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base'
+        onChange={(e)=>setPassword(e.target.value)}
+      className='bg-[#eeeeee]  mb-5 rounded px-4 py-2 border w-full text-lg placeholder:text-base'
       required type="password" placeholder="password"/>
       <button
       className='bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2  w-full text-lg placeholder:text-base'
       >Register</button>
     
       </form>
-      <p className='text-center'> Already an account? <Link  to={'/login'} className='text-blue-600'>Login here </Link></p> 
+      <p className='text-center'> Already have an account? <Link  to={'/login'} className='text-blue-600'>Login here </Link></p> 
       </div>
       <div className="">
-        <Link  
-        to={'/captain-signup'}
-        className='bg-[#10b461] flex items-center justify-center text-white font-semibold mb-5 rounded px-4 py-2  w-full text-lg placeholder:text-base'>Signup as Captain</Link>
+        <p className='text-xs leading-tight'>By proceeding , you consent to get calls, whatsapp, or SMS messages, including by automated means ,from Putoto, and its affliates to the email provided </p>
       </div>
     </div>
   )
